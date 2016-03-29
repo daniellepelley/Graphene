f87be1f1f060e1fc14a0c5572121db9b47ac0c70
daniellepelley@gmail.com <daniellepelley@gmail.com>
2016-03-29 23:16:00 +0100

    Tests for Scalar types and Object type

---
+++ Graphene.Test/TypeTests.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+
+namespace Graphene.Test
+{
+    public class TypeTests
+    {
+        [Test]
+        public void StringTypeTest()
+        {
+            var json = File.ReadAllText("StringType.json");
+
+            var sut = new TypeJsonBuilder();
+
+            var actual = sut.Build("SCALAR", "String");
+
+            Assert.AreEqual(json, actual);
+        }
+
+        [Test]
+        public void BooleanTypeTest()
+        {
+            var json = File.ReadAllText("BooleanType.json");
+
+            var sut = new TypeJsonBuilder();
+
+            var actual = sut.Build("SCALAR", "Boolean");
+
+            Assert.AreEqual(json, actual);
+        }
+
+        [Test]
+        public void ObjectTypeTest()
+        {
+            var json = File.ReadAllText("ObjectType.json");
+
+            var sut = new TypeJsonBuilder();
+
+            var actual = sut.Build("OBJECT", "__Type");
+
+            Assert.AreEqual(json, actual);
+        }
+    }
+}
---
+++ Graphene/Graphene/TypeJsonBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace Graphene
+{
+    public class TypeJsonBuilder
+    {
+        public string Build(string kind, string name)
+        {
+            var jObject = new JObject
+            {
+                {"kind", new JValue(kind)},
+                {"name", new JValue(name)},
+                {"description", new JRaw("null")},
+                {"fields", new JArray()},
+                {"inputFields", new JArray()},
+                {"interfaces", new JArray()},
+                {"enumValues", new JArray()},
+                {"possibleTypes", new JArray()}
+            };
+            return jObject.ToString();
+        }
+    }
+}
